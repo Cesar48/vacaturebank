e945bbf5afe896a992f1f71d69bf0ff8086b8d1b
Cesar <cesar@debijlesmeester.nl>
2020-11-14 18:57:47 +0100

    added to urls

--- vacaturebank/companies/urls.py
+++ vacaturebank/companies/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
 	path('inschrijven', views.signup, name='signup'),
 	path('joboffer/create', views.create_job_offer, name='create_job_offer'),
 	path('joboffer/edit/<int:obj_id>', views.create_job_offer, name='create_job_offer'),
+	path('skills/add/<int:offerid>', views.add_job_skills, name='add_job_skills'),
+	path('skills/types', views.get_skill_types, name='get_skill_types'),
+	path('skills/match', views.get_matching_skills, name='get_matching_skills'),
 ]
